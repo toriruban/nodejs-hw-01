daea2d19491fe7ddf7dcd93bfe6ef2b4a7a4a1c5
ToriRuban <toriruban@icloud.com>
2025-08-04 11:49:43 +0300

    feat: add countContacts script to get all contact in db.json

--- src/scripts/countContacts.js
+++ src/scripts/countContacts.js
@@ -1,3 +1,15 @@
-export const countContacts = async () => {};
+import fs from 'node:fs/promises';
+import { PATH_DB } from '../constants/contacts.js';
+
+export const countContacts = async () => {
+    try {
+        const data = await fs.readFile(PATH_DB, 'utf-8');
+        const contacts = JSON.parse(data);
+        return contacts.length
+
+    } catch (err){
+        throw(err)
+    }
+};
 
 console.log(await countContacts());

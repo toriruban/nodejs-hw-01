50257269d554270fde9642ef453bbf9aba272564
ToriRuban <toriruban@icloud.com>
2025-08-04 11:13:38 +0300

    feat: add addOneContact script to append one contact to db.json

--- src/scripts/addOneContact.js
+++ src/scripts/addOneContact.js
@@ -1,3 +1,23 @@
-export const addOneContact = async () => {};
+import { createFakeContact } from '../utils/createFakeContact.js';
+import { PATH_DB } from '../constants/contacts.js';
+import fs from 'node:fs/promises';
+
+export const addOneContact = async () => {
+ let existingContacts = [];
+    try {
+        const data = await fs.readFile(PATH_DB, 'utf-8');
+        existingContacts = JSON.parse(data)
+    } catch (err){
+        throw(err)
+    }
+ const oneContact = createFakeContact(); 
+  existingContacts.push(oneContact); 
+
+    try {
+        await fs.writeFile(PATH_DB, JSON.stringify(existingContacts), 'utf-8');
+    } catch (err) {
+        throw (err)
+    }
+};
 
 addOneContact();

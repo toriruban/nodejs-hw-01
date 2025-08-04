7671bb74f7e9d73535828cc0aa7fe091553dcbd2
ToriRuban <toriruban@icloud.com>
2025-08-04 09:44:05 +0300

    create: utility function writeContacts

--- src/utils/writeContacts.js
+++ src/utils/writeContacts.js
@@ -1,3 +1,12 @@
 import { PATH_DB } from '../constants/contacts.js';
+import fs from 'node:fs/promises';
 
-export const writeContacts = async (updatedContacts) => {};
+export const writeContacts = async (updatedContacts) => {
+    const data = JSON.stringify(updatedContacts);
+    try {
+        await fs.writeFile(PATH_DB, data, 'utf-8');
+        return updatedContacts;
+    } catch (err){
+        throw err;
+    }
+};

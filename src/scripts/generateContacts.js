ad9e4d8c7874c2aaa855383a0be30a449d2e25e9
ToriRuban <toriruban@icloud.com>
2025-08-04 10:38:56 +0300

    feat: add generateContacts script to append new fake contacts to db.json

--- src/scripts/generateContacts.js
+++ src/scripts/generateContacts.js
@@ -1,3 +1,28 @@
-const generateContacts = async (number) => {};
+import { createFakeContact } from '../utils/createFakeContact.js';
+import { PATH_DB } from '../constants/contacts.js';
+import fs from 'node:fs/promises';
 
+const generateContacts = async (number) => {
+   
+let existingContacts = [];
+   try {
+    const data = await fs.readFile(PATH_DB, 'utf-8');
+    existingContacts = JSON.parse(data); 
+   } catch (err){
+    throw(err)
+   }
+
+const newContacts  = [];
+   for (let i = 0; i < number; i++) { 
+    newContacts.push(createFakeContact()) 
+   }
+
+
+const updateContacts = [...existingContacts, ...newContacts];
+    try {
+        await fs.writeFile(PATH_DB, JSON.stringify(updateContacts), 'utf-8');
+    } catch (err){
+        throw(err);
+    }
+};
 generateContacts(5);

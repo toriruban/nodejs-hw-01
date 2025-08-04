90c012f66a1208bb3f278948d9c92b046c89bea3
ToriRuban <toriruban@icloud.com>
2025-08-04 12:47:45 +0300

    feat: add removeLastContact script to remove last one contact in db.json

--- src/scripts/removeLastContact.js
+++ src/scripts/removeLastContact.js
@@ -1,3 +1,17 @@
-export const removeLastContact = async () => {};
+import fs from 'node:fs/promises';
+import { PATH_DB } from '../constants/contacts.js';
+
+export const removeLastContact = async () => {
+    const data = await fs.readFile(PATH_DB, 'utf-8');
+    const contacts = JSON.parse(data)
+
+    if(contacts.length === 0) { 
+        return [];
+     };
+
+     const removed = contacts.pop();
+     await fs.writeFile(PATH_DB, JSON.stringify(contacts, null, 2), 'utf-8');
+     return removed;
+ };
 
 removeLastContact();

37ee70e1208a35f5a6700dfe2dde158106a10e0e
ToriRuban <toriruban@icloud.com>
2025-08-04 12:07:55 +0300

    feat: add removeAllContacts script to remove all contact in db.json

--- src/scripts/removeAllContacts.js
+++ src/scripts/removeAllContacts.js
@@ -1,3 +1,12 @@
-export const removeAllContacts = async () => {};
+import fs from 'node:fs/promises';
+import { PATH_DB } from '../constants/contacts.js';
 
+export const removeAllContacts = async () => {
+    try {
+        await fs.writeFile(PATH_DB, JSON.stringify([], null, 2), 'utf-8');
+        return [];
+    } catch (err) {
+        throw(err)
+    }
+};
 removeAllContacts();
